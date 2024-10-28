1e56dd36e9caae41d4331ab903860d78ead58cfa
JoannaHChiu <157113266+JoannaHChiu@users.noreply.github.com>
2024-10-28 16:25:03 -0700

    Update input.cpp

--- input.cpp
+++ input.cpp
@@ -1,1 +1,1 @@
-# This is the input functions file! You can decide if you want to split the input and validation portions into two functions or just one.
+// This is the input functions file! You can decide if you want to split the input and validation portions into two functions or just one.
