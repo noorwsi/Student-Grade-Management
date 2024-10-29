9e497451a672ad34097e933f6ca13eed3ad05271
JoannaHChiu <157113266+JoannaHChiu@users.noreply.github.com>
2024-10-28 17:12:58 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,11 @@
 #include <iostream>
+#include "input.hpp"
+#include "addGrades.hpp"
+#include "removeGrades.hpp"
+#include "viewGrades.hpp"
+#include "search.hpp"
+#include "advancedFunctions.hpp"
+
 using namespace std;
 
 // This is the main function! 
